58715e859e8865b22139918bc405c2e5a7a873ec
Andrey <agalkingps@yandex.ru>
2023-11-15 16:26:26 +0300

    Function switchTabs is made

--- order-flow/src/main/java/ru/agalkingps/mealapp/order_flow/Navigation.kt
+++ order-flow/src/main/java/ru/agalkingps/mealapp/order_flow/Navigation.kt
@@ -46,29 +46,36 @@ fun NavigationGraph(navController: NavHostController, userId: Int) {
         composable(Destinations.SelectMealScreen.route) {
             SelectMealScreen(
                 onSelectionDone = {
-                    navController.navigate(Destinations.ShoppingCartScreen.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
-                        }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
+                    navController.switchTabs(Destinations.ShoppingCartScreen.route)
                 }
             )
         }
         composable(Destinations.ShoppingCartScreen.route) {
             ShoppingCartScreen(userId,
                 onGotoPay = {
-                    navController.navigate(Destinations.ProfileScreen.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
-                        }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
+                    navController.switchTabs(Destinations.ProfileScreen.route)
                 }
             )
         }
     }
 }
+
+fun NavHostController.switchTabs(route: String) {
+    navigate(route) {
+        // Pop up to the start destination of the graph to
+        // avoid building up a large stack of destinations
+        // on the back stack as users select items
+        popUpTo(graph.findStartDestination().id) {
+            saveState = true
+        }
+
+        // Avoid multiple copies of the same destination when
+        // reselecting the same item
+        launchSingleTop = true
+
+        // Restore state when reselecting a previously selected item
+        restoreState = true
+    }
+}
+
 
